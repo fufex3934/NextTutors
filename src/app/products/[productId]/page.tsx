00f0ba7bbebc195f1280b591c191927da67534d6
fufex3934 <fufawakgari174@gmail.com>
2025-03-14 14:41:18 +0300

    nested dynamic routes

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,7 @@
 export default async function ProductDetails({
   params,
 }: {
-  params: { productId: string };
+  params: Promise<{ productId: string }>;
 }) {
   const productId =  (await params).productId;
   return <h1>Product details {productId}</h1>;
